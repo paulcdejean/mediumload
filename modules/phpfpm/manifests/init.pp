2536dfce2285dbb20bc14d62d52b2c2ed4dc3bb0
root <root@stg-ac123.appcenter123.com>
2014-09-04 12:41:19 -0400

    Added memcached support to PHP.

--- modules/phpfpm/manifests/init.pp
+++ modules/phpfpm/manifests/init.pp
@@ -14,4 +14,31 @@ class phpfpm {
     mode => 644,
     source => "puppet:///modules/$module_name/php.ini",
   }
+
+  service { 'php-fpm':
+    require => [File['/etc/php.ini'], File['/etc/init.d/php-fpm']],
+    enable => true,
+    subscribe => File['/etc/php.ini'],
+  }
+
+  # Lets get some memcached up in this.
+  package { 'memcached':
+    ensure => installed,
+  }
+
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+  }
+
+  package { 'php-pecl-memcached':
+    ensure => installed,
+    notify => Service['php-fpm'],
+  }
+
+  package { 'php-pecl-memcache':
+    ensure => installed,
+    notify => Service['php-fpm'],
+  }
+  
 }
